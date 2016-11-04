78c02b4daea2cfe0cdf9e1af478b23c927dc5435
Anders Milje <syntox32@gmail.com>
2016-11-04 16:32:27 +0100

    added some debug functions

---
+++ compiler/utils.py
@@ -0,0 +1,18 @@
+
+class Utils:
+    DebugPrint = True
+
+    @staticmethod
+    def debug(string):
+        if Utils.DebugPrint:
+            #name =  inspect.stack()[0][3]
+            name = inspect.currentframe().f_back.f_code.co_name # caller func name
+            print("dbg::\'{0}\'::  {1}".format(name, string))
+
+    @staticmethod
+    def check_none_critical(obj):
+        if obj is None:
+            name = inspect.currentframe().f_back.f_code.co_name # caller func name
+            print("dbg::\'{0}\'::  {1}".format(name, "Critical!: Object was None."))
+            return True
+        return False
